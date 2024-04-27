9e3dc6563ddd9557d55fe62ecedc1e48647e1de2
Ivan <evankirk@vk.com>
2024-04-28 00:58:32 +0500

    Начал класс-клиент RoutingAPI

---
+++ map-peeker/src/api_clients/two_gis_api_clients.py
@@ -0,0 +1,12 @@
+from requests import Session, HTTPError
+import json
+
+from . api_client_interfaces import IGeocoder, I2GISAPIClient
+from .. geolocation import Address, Coordinate
+
+class RoutingAPIClient(I2GISAPIClient):
+    
+    @property
+    def base_url(self):
+        return self._BASE_URL
+    
