4f45feaf05f1edd5e06adb30ea6358f859597ee2
Ivan <evankirk@vk.com>
2024-05-26 00:44:15 +0500

    Модель для окна с картой

--- goat_route/models/map_window_model.py
+++ goat_route/models/map_window_model.py
@@ -1,2 +1,14 @@
-class MapWindowModel:
-    pass
+from .model_interface import IModel
+
+class MapWindowModel(IModel):
+    
+    def _init__(self) -> None:
+
+        super().__init__()
+
+
+    def notify_observers(self) -> None:
+
+        for observer in self._observers:
+            
+            observer.model_is_changed()
