2ba8bcc392e993f2446d1f37e08f44019d6dbfd0
Ivan <evankirk@vk.com>
2024-05-26 00:43:27 +0500

    Модель для главного окна

--- goat_route/models/main_window_model.py
+++ goat_route/models/main_window_model.py
@@ -1,2 +1,13 @@
-class MainWindowModel:
-    pass
+from .model_interface import IModel
+
+class MainWindowModel(IModel):
+
+    def ___init__(self) -> None:
+
+        super()._init__()
+
+    def notify_observers(self):
+
+        for observer in self._observers:
+            
+            observer.model_is_changed()
