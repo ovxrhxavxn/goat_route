2dd00762f1e8e7af32efbf1a4bb0051e0964b3d3
Ivan <evankirk@vk.com>
2024-05-26 00:45:29 +0500

    редставление для главного окна

--- goat_route/views/main_window_view.py
+++ goat_route/views/main_window_view.py
@@ -1,2 +1,14 @@
-class MainWindowView:
-    pass
+from .view_interface import IView
+from ..utilities.observer import Observer
+
+class MainWindowView(IView, Observer):
+
+    def __init__(self, ui, controller, model):
+
+        super().__init__(ui, controller, model)
+
+    def show(self):
+        pass
+
+    def model_is_changed(self):
+        pass
