4390b1948a45cf01d93e7d750b89eedf755e3c1b
Sergey <kliningchst@mail.ru>
2024-05-26 02:26:45 +0300

    Рефакторинг

--- goat_route/controllers/controller_interface.py
+++ goat_route/controllers/controller_interface.py
@@ -1,13 +1,11 @@
 from abc import ABC, abstractmethod
 
-from ..views.view_interface import IView
 from ..models.model_interface import IModel
 
 class IController(ABC):
 
-    def __init__(self, view: IView, model: IModel) -> None:
+    def __init__(self, model: IModel) -> None:
 
-        self._view = view
         self._model = model
 
     @abstractmethod
