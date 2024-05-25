b1f0cff0e4ab1531f4d3b1eed1173a3e876a82ed
Ivan <evankirk@vk.com>
2024-05-26 00:45:50 +0500

    Представление для окна с картой

--- goat_route/views/map_window_view.py
+++ goat_route/views/map_window_view.py
@@ -1,2 +1,14 @@
-class MapWindowView:
-    pass
+from .view_interface import IView
+from ..utilities.observer import Observer
+
+class MapWindowView(IView, Observer):
+
+    def __init__(self, ui, controller, model):
+
+        super().__init__(ui, controller, model)
+
+    def show(self):
+        pass
+
+    def model_is_changed(self):
+        pass
