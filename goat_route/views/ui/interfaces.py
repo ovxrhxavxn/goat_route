ceb5adcac7de300896cbac38cae107ba69b64510
Ivan <evankirk@vk.com>
2024-05-28 02:34:15 +0500

    Добавил owner view

--- goat_route/views/ui/interfaces.py
+++ goat_route/views/ui/interfaces.py
@@ -1,6 +1,12 @@
 from abc import ABC, abstractmethod
 
+from ..interfaces import IView
+
 class IGUI(ABC):
+
+    def set_owner_view(self, view: IView):
+
+        self._owner = view
 
     @abstractmethod
     def init(self):
