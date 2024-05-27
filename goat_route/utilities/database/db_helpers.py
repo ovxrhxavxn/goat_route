7c6aeeb7b2f6940972ac731a3bfc93d5993d7c75
Sergey <kliningchst@mail.ru>
2024-05-28 02:10:53 +0300

    Реализация DBHelper

--- goat_route/utilities/database/db_helpers.py
+++ goat_route/utilities/database/db_helpers.py
@@ -1,7 +1,31 @@
 import sqlite3
-
-from .interfaces import IDBHelper
+from interfaces import IDBHelper
 
 class DBHelperSQLite(IDBHelper):
 
-    pass
+    def __init__(self, database_file):
+        super().__init__(database_file)
+        self.conn = sqlite3.connect(database_file)
+        self.cursor = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS addresses
+                             (id INTEGER PRIMARY KEY AUTOINCREMENT, address TEXT)''')
+
+    def save_address(self, address):
+        self.cursor.execute("INSERT INTO addresses (address) VALUES (?)", (address,))
+        self.conn.commit()
+
+    def get_addresses(self):
+        self.cursor.execute("SELECT * FROM addresses")
+        result = self.cursor.fetchall()
+        return result if result else []
+    
+    def saves_address(self, address):
+        if address.strip():
+            self.cursor.execute("INSERT INTO addresses (address) VALUES (?)", (address,))
+            self.conn.commit()
+
+    def close(self):
+        self.conn.close()
--- goat_route/utilities/database/interfaces.py
+++ goat_route/utilities/database/interfaces.py
@@ -1,7 +1,33 @@
+import sqlite3
 from abc import ABC, abstractmethod
 
 class IDBHelper(ABC):
+    def __init__(self, database_file):
+        self.database_file = database_file
+        self.conn = None
+        self.cursor = None
 
     @abstractmethod
     def create_table(self):
+        """Абстрактный метод для создания таблицы в базе данных."""
         pass
+
+    @abstractmethod
+    def save_address(self, address: str):
+        """Абстрактный метод для сохранения адреса в базе данных."""
+        pass
+
+    @abstractmethod
+    def get_addresses(self):
+        """Абстрактный метод для получения всех адресов из базы данных."""
+        pass
+
+    def close(self):
+        """Метод для закрытия соединения с базой данных."""
+        if self.conn:
+            self.conn.close()
+
+    def connect(self):
+        """Метод для подключения к базе данных."""
+        self.conn = sqlite3.connect(self.database_file)
+        self.cursor = self.conn.cursor()
